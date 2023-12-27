079605794008802697993705823d9f659a4da031
Rohit kumar <57314394+rohit-kumar170@users.noreply.github.com>
2023-12-27 21:54:46 +0530

    Delete ImageUploader directory

--- ImageUploader/myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.db import models
-from  .models import Image
-# Register your models here.
-
-@admin.register(Image)
-
-class ImageAdmin(admin.ModelAdmin):
-    list_display=["id","photo","date"]
